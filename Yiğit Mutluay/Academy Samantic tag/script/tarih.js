3bb540c6f4418355dc38ea7cb6dc00735320136e
KillNoxe <90351908+KillNoxe@users.noreply.github.com>
2021-10-15 14:41:09 +0300

    Add files via upload

---
+++ Yiğit Mutluay/Academy Samantic tag/script/tarih.js
@@ -0,0 +1,2 @@
+const d = new Date();
+document.getElementById("demo").innerHTML = "Copyright &copy; " + d.getFullYear() + " Play Academy";
---
+++ Yiğit Mutluay/deneme/Academy/script/script.js
@@ -0,0 +1,3 @@
+ const d = new Date();
+ document.getElementById("demo").innerHTML = "Copyright &copy; " + d.getFullYear() + " Play Academy";
+ 
